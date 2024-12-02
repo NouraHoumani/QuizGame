fc814f1af64d71201172c129448fe90d43a5d70c
NouraHoumani <nourahoumani@outlook.com>
2024-12-02 21:40:17 +0200

    second commit

--- QuizGame1/app/src/main/java/com/techmania/quizgame1/MainActivity.kt
+++ QuizGame1/app/src/main/java/com/techmania/quizgame1/MainActivity.kt
@@ -10,37 +10,35 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.techmania.quizgame1.databinding.ActivityMainBinding
 import com.techmania.quizgame1.ui.theme.QuizGame1Theme
 
+
+import android.content.Intent
+import com.google.firebase.auth.FirebaseAuth
+
 class MainActivity : ComponentActivity() {
+
+    lateinit var mainBinding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            QuizGame1Theme {
-                // Main Content Screen
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+
+        mainBinding=ActivityMainBinding.inflate((layoutInflater))
+        val view =mainBinding.root
+
+
+       setContentView(view)
+
+
+            mainBinding.buttonSignOut.setOnClickListener{
+
+                FirebaseAuth.getInstance().signOut()
+                val intent=Intent(this,LoginActivity::class.java)
+                startActivity(intent)
+                finish()
+
             }
-        }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    QuizGame1Theme {
-        Greeting("Android")
-    }
-}
--- QuizGame1/app/src/main/java/com/techmania/quizgame1/SignupActivity.kt
+++ QuizGame1/app/src/main/java/com/techmania/quizgame1/SignupActivity.kt
@@ -6,28 +6,60 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import android.content.Intent
+import android.view.View
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 import com.techmania.quizgame1.databinding.ActivitySignupBinding
+import com.google.firebase.FirebaseApp
+
+import kotlin.math.sign
 
 class SignupActivity : AppCompatActivity() {
 
     lateinit var signupBinding: ActivitySignupBinding
+
+    val auth : FirebaseAuth = FirebaseAuth.getInstance()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        signupBinding=ActivitySignupBinding.inflate(layoutInflater)
-
-        val view=signupBinding.root
-        enableEdgeToEdge()
+        signupBinding = ActivitySignupBinding.inflate(layoutInflater)
+        val view = signupBinding.root
         setContentView(view)
 
-        signupBinding.buttonSignup.setOnClickListener{
+        signupBinding.buttonSignup.setOnClickListener {
+
+            val email = signupBinding.editTextSignupEmail.text.toString()
+            val password = signupBinding.editTextSignupPassword.text.toString()
+
+            signupWithFirebase(email, password)
 
         }
 
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
     }
+
+    fun signupWithFirebase(email : String,password : String){
+
+        signupBinding.progressBarSignup.visibility = View.VISIBLE
+        signupBinding.buttonSignup.isClickable = false
+
+        auth.createUserWithEmailAndPassword(email, password).addOnCompleteListener { task ->
+
+            if (task.isSuccessful){
+
+                Toast.makeText(applicationContext,"Your account has been created",Toast.LENGTH_SHORT).show()
+                finish()
+                signupBinding.progressBarSignup.visibility = View.INVISIBLE
+                signupBinding.buttonSignup.isClickable = true
+
+            }else{
+
+                Toast.makeText(applicationContext,task.exception?.localizedMessage,Toast.LENGTH_SHORT).show()
+
+            }
+
+        }
+
+    }
+
 }
+
